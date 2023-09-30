9212f3b7eacb52bb44498f8a38b94fb81a5e35c3
RostyslavBesh <8481681@stud.nau.edu.ua>
2023-09-30 19:01:57 +0300

    Delete the variable "c" because it useless

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSHARP
+{
+    class Program
+    {
+        static void Clever(int c)
+        {
+            Console.WriteLine(c);
+
+        }
+        static void Learn(int a,int b)
+        {
+            int  c=0;
+            for (int i = 0; i < a; i++)
+            {
+                c =c+ a + b;
+            }
+            Clever(c);
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Learn(a, b);
+        }
+    }
+}
