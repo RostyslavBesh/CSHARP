cf33f0fb7420387719339e87f4cc0d72fb158529
RostyslavBesh <8481681@stud.nau.edu.ua>
2023-09-30 19:38:11 +0300

    commi

--- Program.cs
+++ Program.cs
@@ -24,7 +24,7 @@ namespace CSHARP
             a = int.Parse(Console.ReadLine());
             b = int.Parse(Console.ReadLine());
             Learn(a, b);
-            Console.WriteLine("1231232");
+            Console.WriteLine("1231231231231232");
         }
     }
 }
